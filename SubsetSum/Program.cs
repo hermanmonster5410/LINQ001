3e3919746cf0249d8d597441bc5c15760024dd4f
Yuriy Gruzglin <gruzglinyy@gmail.com>
2018-10-24 02:50:57 -0400

    More

---
+++ SubsetSum/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsetSum
+{
+    class Program
+    {
+        int[] m = new int[] { 1, 3, 5, 10, 20, 8, 14 };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Result: " + IsSubsetSum(m, 28));
+            Console.WriteLine("Done");
+        }
+
+
+        public static bool IsSubsetSum(int[] ar, int s)
+        {
+            int[] ar0 = ar.Where(a => a != s).ToArray();
+            if (ar0.Length != ar.Length)
+                return true;
+
+            if (ar0.Length == 2 && ar0[0] + ar0[1] == s)
+                return true;
+
+            for ( int i = 0; i < ar0.Length; i++ )
+            {
+                int[] w = new int[ar0.Length - 1];
+                int cur = 0;
+                int s0=0;
+                for ( int j = 0; j < w.Length; j++)
+                {
+                    if (j == i)
+                        s0 = s - ar0[j];
+                    else
+                    {
+                        w[cur++] = ar0[j];
+                    }
+                }
+
+                if (IsSubsetSum(w, s0) == true)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsSubsetSumX(int[] ar, int s)
+        {
+
+            return false;
+        }
+    }
+}
