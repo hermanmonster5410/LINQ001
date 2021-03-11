5bd4aa45fd45cdc543822a840d2b95468d3c94b4
gruzglinam <mayya.gruzglina@outlook.com>
2021-03-10 19:54:11 -0500

    Immutable collections added

---
+++ ImmutableCollections/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ImmutableCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+
+
+        }
+
+
+        public static IReadOnlyDictionary<string, int> GetReports(DateTime? from, DateTime? to)
+        {
+            Dictionary<string, int> dct = new Dictionary<string, int>();
+
+            return new ReadOnlyDictionary<string, int>(dct);
+        }
+    }
+
+    public class Invoice
+    {
+        public int Id;
+        public string Description;
+        public DateTime CreateDate;
+        public DateTime? PayDate;
+        public IList<InvoiceItem> InvoiceItems;
+    }
+
+    public class InvoiceItem
+    {
+        public int Count;
+        public decimal Price;
+        public string ProductId;
+    }
+}
